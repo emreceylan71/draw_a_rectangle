9c5f52b278305662a46da0d3175b99f9344c20f5
emreceylan71 <109828939+emreceylan71@users.noreply.github.com>
2022-08-08 22:30:09 +0300

    Add files via upload

---
+++ dikdortgen_cizme.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int begin1, vertical1, wide2,length3;
+	cout << "Enter a position where rectengle begin horizontally" << endl;
+	cin >> begin1;
+	cout << "Enter a position where rectengle begin vertically" << endl;
+	cin >> vertical1;
+	cout << "Enter a rectangle wide" << endl;
+	cin >> wide2;
+	cout << "Enter a rectange length" << endl;
+	cin >> length3;
+	
+	for (int i = 1; i <= vertical1; i++) {// this for loop doing vertical begin
+		cout << "\n";
+	  }
+
+	  for(int i=0;i<length3;i++){
+		  
+		  cout << "\n";// this line makes it go down to a bottom line in each loop and determines the length.
+
+		  for (int i = 1; i <= begin1; i++) {// this for loop doing horizontal begin
+			  cout << " ";
+		  }
+
+
+	     for (int i=0; i < wide2; i++) {// it makes the wide
+		cout << "*";
+         }
+
+
+	  }
+	
+}
+
